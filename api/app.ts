673c8ddccbc3b1f31cf814395878f599f8aef62d
Juan Vanegas <jandvanegas@gmail.com>
2022-05-16 08:47:00 +0200

    bugfix(api): fixed port binding.

--- api/app.ts
+++ api/app.ts
@@ -16,7 +16,7 @@ import {ErrorHandler} from "./src/ErrorHandler";
 
 const app: express.Application = express();
 const server: http.Server = http.createServer(app);
-const port = 3000;
+const port = process.env.PORT || 3000;
 const routes: Array<CommonRoutesConfig> = [];
 const debugLog: debug.IDebugger = debug('app');
 
