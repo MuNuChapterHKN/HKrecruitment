837f31a841e1ce19697030619b07411a3b10e651
Alberto Baroso <albertobaroso999@gmail.com>
2023-04-09 20:56:56 +0200

    Updated shared application validation schemas

--- shared/src/application.ts
+++ shared/src/application.ts
@@ -10,10 +10,13 @@ export enum ApplicationState {
   RefusedByApplicant = "refused_by_applicant",
 }
 
+export enum ApplicationType {
+  BSC = "bsc",
+  MSC = "msc",
+  PHD = "phd",
+}
+
 export enum LangLevel {
-  // A1 = "A1",
-  // A2 = "A2",
-  // B1 = "B1",
   B2 = "B2",
   C1 = "C1",
   C2 = "C2",
@@ -21,36 +24,37 @@ export enum LangLevel {
 }
 
 export interface Application {
-  id: string;
-  submission: Date;
+  type: ApplicationType;
+  id: number;
   state: ApplicationState;
-  lastModified: Date;
   notes?: string;
   cv: string; // Link to cv
-  // availability: TimeSlot[];
   itaLevel: LangLevel;
+
+  bscApplication?: BscApplication;
+  mscApplication?: MscApplication;
+  phdApplication?: PhdApplication;
 }
 
-export interface BscApplication extends Application {
-  studyPath: string;
-  academicYear: number;
-  cfu: number;
+export interface BscApplication {
+  bscStudyPath: string;
+  bscAcademicYear: number;
+  bscGradesAvg: number;
   grades: string; // Link to grades
-  gradesAvg: number;
+  cfu: number;
 }
 
-export interface MscApplication extends Application {
+export interface MscApplication {
   bscStudyPath: string;
   bscGradesAvg: number;
-
   mscStudyPath: string;
   mscGradesAvg: number;
   academicYear: number;
-  cfu: number;
   grades: string; // Link to grades
+  cfu: number;
 }
 
-export interface PhdApplication extends Application {
+export interface PhdApplication {
   mscStudyPath: string;
   phdDescription: string;
 }
@@ -58,55 +62,66 @@ export interface PhdApplication extends Application {
 /* Validation schemas */
 
 const BaseApplication = Joi.object<Application>({
-  submission: Joi.date().required(),
-  state: Joi.string().valid(Object.values(ApplicationState)).required(),
-  lastModified: Joi.date().required(),
+  type: Joi.string()
+    .valid(...Object.values(ApplicationType))
+    .required(),
   notes: Joi.string().optional(),
   cv: Joi.string().uri().required(),
-  itaLevel: Joi.string().valid(Object.values(LangLevel)).required(),
-
-  // availability: Joi.array().items(Joi.object<TimeSlot>({
-  //     start: Joi.date().required(),
-  //     end: Joi.date().required(),
-  // })).required(),
+  itaLevel: Joi.string()
+    .valid(...Object.values(LangLevel))
+    .required(),
 });
 
-const createBscApplication = (
-  BaseApplication as Joi.ObjectSchema<BscApplication>
-).keys({
-  studyPath: Joi.string().required(),
-  academicYear: Joi.number().required(),
-  cfu: Joi.number().required(),
+const createBscApplication = Joi.object<BscApplication>({
+  bscStudyPath: Joi.string().max(255).required(),
+  bscAcademicYear: Joi.number().integer().min(1).max(3).required(),
+  bscGradesAvg: Joi.number().min(18).max(30).required(),
+  cfu: Joi.number().integer().min(48).max(180).required(),
   grades: Joi.string().uri().required(),
-  gradesAvg: Joi.number().required(),
 });
 
-const createMscApplication = (
-  BaseApplication as Joi.ObjectSchema<MscApplication>
-).keys({
+const createMscApplication = Joi.object<MscApplication>({
   bscStudyPath: Joi.string().optional(),
-  bscGradesAvg: Joi.number().optional(),
+  bscGradesAvg: Joi.number().min(18).max(30).optional(),
   mscStudyPath: Joi.string().required(),
-  mscGradesAvg: Joi.number().required(),
-  academicYear: Joi.number().required(),
-  cfu: Joi.number().required(),
+  mscGradesAvg: Joi.number().min(18).max(30).required(),
+  academicYear: Joi.number().integer().min(1).max(2).required(),
+  cfu: Joi.number().integer().min(20).max(120).required(),
   grades: Joi.string().uri().required(),
 });
 
-const createPhdApplication = (
-  BaseApplication as Joi.ObjectSchema<PhdApplication>
-).keys({
+const createPhdApplication = Joi.object<PhdApplication>({
   mscStudyPath: Joi.string().required(),
-  phdDescription: Joi.string().required(),
+  phdDescription: Joi.string().max(255).required(),
 });
 
-export const createApplicationSchema = Joi.alternatives(
-  createBscApplication,
-  createMscApplication,
-  createPhdApplication
-);
+export const createApplicationSchema = BaseApplication.keys({
+  bscApplication: createBscApplication.when("type", {
+    is: "bsc",
+    then: Joi.required(),
+    otherwise: Joi.forbidden(),
+  }),
+  mscApplication: createMscApplication.when("type", {
+    is: "msc",
+    then: Joi.required(),
+    otherwise: Joi.forbidden(),
+  }),
+  phdApplication: createPhdApplication.when("type", {
+    is: "phd",
+    then: Joi.required(),
+    otherwise: Joi.forbidden(),
+  }),
+})
+  .label("type")
+  .options({
+    stripUnknown: true,
+    abortEarly: false,
+    presence: "required", // TODO: Check .required() on other Schemas
+  });
 
 export const updateApplicationSchema = Joi.object<Application>({
   notes: Joi.string().optional(),
-  state: Joi.string().valid(Object.values(ApplicationState)).optional(),
+  state: Joi.string()
+    .valid(...Object.values(ApplicationState))
+    .optional(),
 });
