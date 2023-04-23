e84d4c8a168565b59a468f9f5408c069d1a740be
Alberto Baroso <albertobaroso999@gmail.com>
2023-04-23 18:01:48 +0200

    refactor(api): Switched from fastify to express

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,16 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-import {
-  FastifyAdapter,
-  NestFastifyApplication,
-} from '@nestjs/platform-fastify';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestFastifyApplication>(
-    AppModule,
-    new FastifyAdapter(),
-  );
+  const app = await NestFactory.create(AppModule);
 
   app.enableCors();
   app.setGlobalPrefix('v1');
