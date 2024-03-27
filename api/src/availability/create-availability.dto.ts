25553d85025fbb8f313ac94ed34a7554c84db18a
Alberto Baroso <albertobaroso999@gmail.com>
2024-03-27 21:53:16 +0100

    fix!: removed unnecessary fields in CreateAvailabilityDto

--- api/src/availability/create-availability.dto.ts
+++ api/src/availability/create-availability.dto.ts
@@ -1,18 +1,6 @@
-import {
-  Availability,
-  AvailabilityState,
-  TimeSlot,
-} from '@hkrecruitment/shared';
 import { ApiProperty } from '@nestjs/swagger';
-import { User } from 'src/users/user.entity';
 
-export class CreateAvailabilityDto implements Availability {
+export class CreateAvailabilityDto {
   @ApiProperty()
-  state: AvailabilityState;
-  @ApiProperty()
-  lastModified: Date;
-  @ApiProperty()
-  timeSlot: TimeSlot;
-  @ApiProperty()
-  user: User;
+  timeSlotId: number;
 }
