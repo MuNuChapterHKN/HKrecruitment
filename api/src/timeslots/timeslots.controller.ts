c9c30fbd83e2bb018da3dd6c6919f8ad1217d029
white <pasquale03.bianco@gmail.com>
2024-05-30 11:29:30 +0200

    feat: check ability

--- api/src/timeslots/timeslots.controller.ts
+++ api/src/timeslots/timeslots.controller.ts
@@ -6,6 +6,8 @@ import {
   ApiUnauthorizedResponse,
 } from '@nestjs/swagger';
 import { TimeSlot } from './timeslot.entity';
+import { CheckPolicies } from 'src/authorization/check-policies.decorator';
+import { Action } from '@hkrecruitment/shared';
 
 @ApiBearerAuth()
 @ApiTags('timeslots')
@@ -14,6 +16,7 @@ export class TimeSlotsController {
   constructor(private readonly timeSlotsService: TimeSlotsService) {}
 
   @ApiUnauthorizedResponse()
+  @CheckPolicies((ability) => ability.can(Action.Read, 'TimeSlot'))
   @Get()
   async findAvailableTimeSlots(): Promise<TimeSlot[]> {
     return await this.timeSlotsService.findAvailableTimeSlots();
