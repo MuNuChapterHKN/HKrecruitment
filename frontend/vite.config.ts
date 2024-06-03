47d71e89625148c9d62b6b88ea7c74984a3e9545
Alberto Baroso <albertobaroso999@gmail.com>
2024-06-03 19:06:27 +0200

    feat: enabled env variables

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
+
+const env = loadEnv(process.env.NODE_ENV ?? 'development', process.cwd(), '');
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  define: {
+    'process.env.REACT_APP_API_ENDPOINT': JSON.stringify(env.REACT_APP_API_ENDPOINT)
+  },
   plugins: [react()],
 });
