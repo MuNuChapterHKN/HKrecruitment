b26879672192459f2fe5ec40f8120224f7d3598a
Giuseppe Pascale <giuseppe.pascale03@gmail.com>
2025-09-24 15:35:54 +0200

    feat: base better-auth

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,8 +1,3 @@
 import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
 
-export const usersTable = pgTable("users", {
-  id: integer().primaryKey().generatedAlwaysAsIdentity(),
-  name: varchar({ length: 255 }).notNull(),
-  age: integer().notNull(),
-  email: varchar({ length: 255 }).notNull().unique(),
-});
+export * from './auth-schema';
---
+++ src/lib/server/auth.ts
@@ -0,0 +1,12 @@
+import { db } from "@/db";
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+
+export const auth = betterAuth({
+  database: drizzleAdapter(db, {
+    provider: 'pg'
+  }),
+  emailAndPassword: {
+    enabled: true,
+  },
+});
