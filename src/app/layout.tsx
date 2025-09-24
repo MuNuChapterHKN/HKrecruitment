03e946865bbc783c62895736244a8b400799685f
Giuseppe Pascale <giuseppe.pascale03@gmail.com>
2025-09-24 15:24:48 +0200

    refactor: cleaning

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, Poppins } from "next/font/google";
+import { Geist_Mono, Poppins } from "next/font/google";
 import "./globals.css";
 
 const poppins = Poppins({
