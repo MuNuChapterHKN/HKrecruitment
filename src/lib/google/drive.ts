a6ad8ee76c0521d23702e730f86c936cb787757b
Giuseppe Pascale <giuseppe.pascale03@gmail.com>
2025-09-25 17:35:06 +0200

    feat: base uploadFile

--- src/lib/google/drive.ts
+++ src/lib/google/drive.ts
@@ -1,11 +1,7 @@
 import { google } from 'googleapis';
 import { Result, ok, err, fromPromise } from 'neverthrow';
 import { service } from './service';
-
-interface CreateFolderParams {
-  name: string;
-  parentId?: string;
-}
+import stream from 'stream';
 
 interface DriveFolder {
   id: string;
@@ -13,8 +9,18 @@ interface DriveFolder {
   parents?: string[];
 }
 
+export interface DriveFile {
+  id: string;
+  name: string;
+  parents?: string[];
+  mimeType: string;
+}
+
 export async function createFolder(
-  params: CreateFolderParams
+  params: {
+    name: string;
+    parentId?: string;
+  }
 ): Promise<Result<DriveFolder, Error>> {
   const drive = google.drive({ version: 'v3', auth: service.auth });
 
@@ -42,6 +48,52 @@ export async function createFolder(
       id: response.data.id,
       name: response.data.name || params.name,
       parents: response.data.parents || undefined
+    });
+  });
+}
+
+export async function uploadFile(
+  params: {
+    file: {
+      name: string;
+      data: ArrayBuffer;
+      type: string;
+    },
+    parentId?: string;
+  }
+): Promise<Result<DriveFile, Error>> {
+  const drive = google.drive({ version: 'v3', auth: service.auth });
+  const { file } = params;
+
+  const fileMetadata: any = {
+    name: file.name
+  };
+
+  if (params.parentId)
+    fileMetadata.parents = [params.parentId];
+
+  const buffer = Buffer.from(file.data);
+
+  return fromPromise(
+    drive.files.create({
+      requestBody: fileMetadata,
+      media: {
+        mimeType: file.type,
+        body: new stream.PassThrough().end(buffer)
+      },
+      fields: 'id, name, parents, mimeType'
+    }),
+    (error) => error instanceof Error ? error : new Error('Unknown error occurred')
+  ).andThen((response) => {
+    if (!response.data.id) {
+      return err(new Error('Failed to upload file: No ID returned'));
+    }
+
+    return ok({
+      id: response.data.id,
+      name: response.data.name || params.file.name,
+      parents: response.data.parents || undefined,
+      mimeType: response.data.mimeType || params.file.type
     });
   });
 }
