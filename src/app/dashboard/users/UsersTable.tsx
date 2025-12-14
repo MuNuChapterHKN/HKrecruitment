fdb1cb34728cd86238c3602879aa4d6bc7a98961
Cristian <s300461@studenti.polito.it>
2025-12-14 23:18:00 +0100

    feat: First Interview column added to users table

--- src/app/dashboard/users/UsersTable.tsx
+++ src/app/dashboard/users/UsersTable.tsx
@@ -4,7 +4,7 @@ import { useState } from 'react';
 import { Search } from 'lucide-react';
 import { Input } from '@/components/ui/input';
 import { AuthUserRole, AuthUserRoleName } from '@/lib/auth';
-
+import { toggleIsFirstTimeCheckbox } from '@/app/actions/users';
 import {
   Select,
   SelectTrigger,
@@ -14,6 +14,7 @@ import {
 } from '@/components/ui/select';
 import { Avatar, AvatarImage, AvatarFallback } from '@/components/ui/avatar';
 import { Button } from '@/components/ui/button';
+import { Checkbox } from '@/components/ui/checkbox';
 
 interface UserDB {
   id: string;
@@ -24,6 +25,7 @@ interface UserDB {
   createdAt: Date;
   updatedAt: Date;
   role: number | null;
+  isFirstTime: boolean;
 }
 
 interface UsersTableProps {
@@ -62,7 +64,7 @@ export default function UsersTable({ users: initialUsers }: UsersTableProps) {
           {/* Header row for search + role filter */}
           <thead>
             <tr>
-              <th colSpan={4} className="p-4 bg-white">
+              <th colSpan={5} className="p-4 bg-white">
                 <div className="flex justify-center items-center relative">
                   {/* Search bar */}
                   <div className="w-full max-w-md">
@@ -126,9 +128,10 @@ export default function UsersTable({ users: initialUsers }: UsersTableProps) {
 
             {/* Columns row */}
             <tr className="bg-gray-100 text-left">
-              <th className="p-3">Profile</th>
-              <th className="p-3">Name</th>
-              <th className="p-3">Email</th>
+              <th className="p-3 pl-4 pr-10">Profile</th>
+              <th className="p-3 w-40">Name</th>
+              <th className="p-3 w-52">Email</th>
+              <th className="p-3 w-32 pr-20 text-center">First Interview</th>
               <th className="p-3">Role</th>
             </tr>
           </thead>
@@ -137,7 +140,7 @@ export default function UsersTable({ users: initialUsers }: UsersTableProps) {
           <tbody>
             {usersToShow.map((user) => (
               <tr key={user.id} className="border-t">
-                <td className="p-3">
+                <td className="p-3 pl-4 pr-10 w-20">
                   <Avatar>
                     <AvatarImage src={user.image || undefined} />
                     <AvatarFallback>
@@ -150,9 +153,32 @@ export default function UsersTable({ users: initialUsers }: UsersTableProps) {
                     </AvatarFallback>
                   </Avatar>
                 </td>
-                <td className="p-3">{user.name}</td>
-                <td className="p-3">{user.email}</td>
-                <td className="p-3 w-40">
+                <td className="p-3 w-40">{user.name}</td>
+                <td className="p-3 w-52">{user.email}</td>
+                <td className="p-3 w-32">
+                  <div className="flex pr-20 justify-center">
+                    <Checkbox
+                      checked={user.isFirstTime}
+                      onCheckedChange={async () => {
+                        // Update the db
+                        await toggleIsFirstTimeCheckbox(
+                          user.id,
+                          user.isFirstTime
+                        );
+
+                        // Update local status immediately for visual feedback (optimistic update)
+                        setUsers(
+                          users.map((u) =>
+                            u.id === user.id
+                              ? { ...u, isFirstTime: !u.isFirstTime }
+                              : u
+                          )
+                        );
+                      }}
+                    />
+                  </div>
+                </td>
+                <td className="p-3 w-32">
                   <Select
                     value={
                       user.role !== null ? AuthUserRoleName[user.role] : ''
@@ -175,14 +201,14 @@ export default function UsersTable({ users: initialUsers }: UsersTableProps) {
 
             {/* Separator line below the last row of the table */}
             <tr>
-              <td colSpan={4}>
+              <td colSpan={5}>
                 <hr className="border-t border-gray-200" />
               </td>
             </tr>
 
             {/* Pagination row */}
             <tr>
-              <td colSpan={4} className="p-4 bg-white">
+              <td colSpan={5} className="p-4 bg-white">
                 <div className="flex justify-between items-center">
                   {/* Showing X of Y */}
                   <div className="flex items-center space-x-2 text-sm text-gray-700">
