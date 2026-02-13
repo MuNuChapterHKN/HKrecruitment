6f479221324fb28cb18d18ea4ae1bd9786af6d2a
Giuseppe Pascale <giuseppe.pascale03@gmail.com>
2026-02-13 16:08:30 +0100

    fix: preparing for deployment

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,19 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+  basePath: '/recruitment',
   output: 'standalone',
   experimental: {
     authInterrupts: true,
+  },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/signin',
+        permanent: true,
+      },
+    ];
   },
 };
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,7 @@
-import { listAllUsers } from '@/lib/services/users';
-
-export const dynamic = 'force-dynamic';
-
 export default async function Home() {
-  const users = await listAllUsers();
-
   return (
     <div>
-      {users.map((u) => (
-        <h2 key={u.id}>{u.name}</h2>
-      ))}
+      <h1>You%apos;ll be redirected shortly...</h1>
     </div>
   );
 }
